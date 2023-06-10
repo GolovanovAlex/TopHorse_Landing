2c2c6f86005350ab0c239816891cb0f02710083e
GolovanovAlex <Golovanov.a.a@yandex.ru>
2023-06-10 08:19:29 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.projects__swiper-text', {
+  slidesPerView: 3,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+const swiper2 = new Swiper('.projects__swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  keyboard: true,
+  thumbs: {
+    swiper: swiper,
+  },
+});
+const swiperGallery = new Swiper('.gallery-swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next-gallery',
+    prevEl: '.swiper-button-prev-gallery',
+  },
+});
---
+++ info.md
@@ -0,0 +1,13 @@
+# Проект "".
+
+npm i
+gulp - start coding
+gulp styles - style scss
+gulp watching - watch for
+gulp browsersync - liveserver
+gulp scripts - scripts
+gulp images - decompress images
+gulp build - build new docs
+gulp cleanDist -
+gulp finish - finish project
+
