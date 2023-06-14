320d52305660291117010ef699310335f4f0c608
GolovanovAlex <Golovanov.a.a@yandex.ru>
2023-06-14 12:00:44 +0300

    scroll

--- app/js/main.js
+++ app/js/main.js
@@ -20,3 +20,18 @@ yesBtn.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
   modal.classList.remove('active');
 });
+
+// ---- ---- Scroll---- ---- //
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
